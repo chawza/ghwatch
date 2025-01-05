8bcdbf4e9fbae006f7120174f5fa55e123979f70
Nabeel KM <nabeelkahlil403@gmail.com>
2025-01-05 11:54:41 +0700

    first gui

--- task-tracker/app.py
+++ task-tracker/app.py
@@ -1,32 +1,5 @@
-from . import github
-from .gui import TaskTrackerApp
+from .gui import GHWatchApp
 
 def app():
-    _app = TaskTrackerApp()
+    _app = GHWatchApp()
     _app.run()
-
-    # for issue in user.get_issues(sort='-created'):
-
-    #     if len(assigned_issues) <= 10:
-    #         if issue.assignee == user:
-    #             assigned_issues.append(issue)
-
-    #     if count >= 100:
-    #         break
-
-    # pulls = []
-    # for pr in user.get_pulls(sort='-created'):
-    #     if len(pulls) <= 10:
-    #         if pr.assignee == user:
-    #             pulls.append(pr)
-    #     if count >= 50:
-    #         break
-
-
-
-    # # print('assigned assues', len(assigned_issues))
-    # # for issue in assigned_issues:
-    # #     print(issue)
-
-    # for pull in pulls:
-    #     print(pull)
--- task-tracker/gui.py
+++ task-tracker/gui.py
@@ -1,27 +1,116 @@
+import os
 from textual.app import App
-from textual.containers import Vertical
-from textual.widgets import Static, TextArea, Button
+from textual.widgets import Static, Input, Header, ListView, ListItem
 from textual.screen import Screen
 from textual import events
 
-class WelcomeScreen(Screen):
+from github.Issue import Issue
+from github import Github
+from github.GithubException import BadCredentialsException
+
+from .import github
+
+class BaseScreen(Screen):
+    app: 'GHWatchApp'
+
+class WelcomeScreen(BaseScreen):
+
     def compose(self):
         yield Static("Enter Github Userrname")
-        yield TextArea(id="username_input")
-        yield Button('Submit', id="submit_button")
-
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "submit_button":
-            self.check_login()
+        yield Input(id="token_input", type='text')
+        yield Static(id='error_message')
 
     def on_key(self, event: events.Key) -> None:
         if event.key == "enter":
-            self.check_login()
+            token = self.query_one("#token_input").value
 
-    def check_login(self):
-        username = self.query_one("#username_input").text
-        self.mount(Static(f'Entered: {username}'))
+            if token:
+                self.check_login(token)
 
-class TaskTrackerApp(App):
+    def on_mount(self):
+        if token := os.environ.get('GITHUB_TOKEN'):
+            self.check_login(token)
+
+
+    # hnadlers
+    def check_login(self, token: str):
+
+        client = github.get_github_client(token)
+        user = client.get_user()
+
+        error_widget: Static = self.query_one('#error_message')
+        error_widget.update('')
+
+        try:
+            user.login
+        except BadCredentialsException:
+            error_widget.update('Invalid username or token')
+            return
+        
+        self.app.client = client
+        self.app.pop_screen()        
+        self.app.push_screen(HomeScreen())
+
+
+class SelectRepoScreen(BaseScreen):
     def compose(self):
-        yield WelcomeScreen()
+        yield Header("Select a repository")
+        # TODO: list repositories and allow selection
+
+
+class SelectIssueScreen(BaseScreen):
+    def __init__(self, repo: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.repo = repo
+
+    def compose(self):
+        yield Header("Select Issue for repo {}".format(self.repo))
+
+
+class HomeScreen(BaseScreen):
+
+    class IssueItem(ListItem):
+        def __init__(self, issue: Issue, *args, **kwargs):
+            self.issue = issue
+            super().__init__(*args, **kwargs)
+
+        def compose(self):
+            yield Static(f'#{self.issue.number} - {self.issue.title}')
+
+    def compose(self):
+        yield Static(f'[{self.app.user.name}]')
+        yield ListView(id='issue_list')
+        yield Static(id='bottom_message')
+
+    def on_mount(self):
+        message = self.query_one('#bottom_message')
+        message.update('Fetching...')
+
+        list_view: ListView = self.query_one('#issue_list')
+        list_view.clear()
+        items = [self.IssueItem(issue) for issue in self.get_latest_issues()]
+
+        if len(items) == 0:
+            message.update('No issues found')
+
+        list_view.clear()
+        list_view.extend(items)
+
+        message.update('')
+
+    def get_latest_issues(self, num_issues=10):
+        return list(self.app.user.get_issues(sort='-created')[:num_issues])
+
+
+class GHWatchApp(App):
+    client: Github
+
+    def on_mount(self):
+        self.push_screen(WelcomeScreen())
+
+
+    @property
+    def user(self):
+        return self.client.get_user()
+
+
